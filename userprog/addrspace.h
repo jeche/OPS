9b6be710da676047e77d22073b24a1d0608729b8
jeche <jlchen@email.wm.edu>
2013-11-04 19:59:58 -0500

    push up

--- userprog/addrspace.h
+++ userprog/addrspace.h
@@ -1,11 +1,11 @@
 #ifndef CHANGED
 // addrspace.h 
-//	Data structures to keep track of executing user programs 
-//	(address spaces).
+//  Data structures to keep track of executing user programs 
+//  (address spaces).
 //
-//	For now, we don't keep any information about address spaces.
-//	The user level CPU state is saved and restored in the thread
-//	executing the user program (see thread.h).
+//  For now, we don't keep any information about address spaces.
+//  The user level CPU state is saved and restored in the thread
+//  executing the user program (see thread.h).
 //
 // Copyright (c) 1992-1993 The Regents of the University of California.
 // All rights reserved.  See copyright.h for copyright notice and limitation 
@@ -17,30 +17,30 @@
 #include "copyright.h"
 #include "filesys.h"
 
-#define UserStackSize		1024 	// increase this as necessary!
+#define UserStackSize   1024  // increase this as necessary!
 
 class AddrSpace {
   public:
-    AddrSpace(OpenFile *executable);	// Create an address space,
-					// initializing it with the program
-					// stored in the file "executable"
-    ~AddrSpace();			// De-allocate an address space
+    AddrSpace(OpenFile *executable);  // Create an address space,
+          // initializing it with the program
+          // stored in the file "executable"
+    ~AddrSpace();     // De-allocate an address space
 
-    void InitRegisters();		// Initialize user-level CPU registers,
-					// before jumping to user code
+    void InitRegisters();   // Initialize user-level CPU registers,
+          // before jumping to user code
 
-    void SaveState();			// Save/restore address space-specific
-    void RestoreState();		// info on a context switch 
+    void SaveState();     // Save/restore address space-specific
+    void RestoreState();    // info on a context switch 
     bool ReadMem(int addr, int size, int *value);
     bool WriteMem(int addr, int size, int value);
     ExceptionType Translate(int virtAddr, int* physAddr, int size, bool writing);
 
   private:
 #ifndef USE_TLB
-    TranslationEntry *pageTable;	// Assume linear page table translation
-#endif					// for now!
-    unsigned int numPages;		// Number of pages in the virtual 
-					// address space
+    TranslationEntry *pageTable;  // Assume linear page table translation
+#endif          // for now!
+    unsigned int numPages;    // Number of pages in the virtual 
+          // address space
 };
 
 #endif // ADDRSPACE_H
@@ -119,6 +119,8 @@ class AddrSpace {
     int child;
     int parent;
     int sibling;
+    int exit;
+    // Semaphore* death;
     FileShield** fileDescriptors;
     AddrSpace(OpenFile *executable);  // Create an address space,
           // initializing it with the program
