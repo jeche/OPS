744dddcd3fbdb0dbbc3c0b53fa38dd1892d963d6
Andrew T  Jones <atjones@email.wm.edu>
2013-11-10 15:51:22 -0500

    shell sortaworking

--- test/shell.c
+++ test/shell.c
@@ -79,20 +79,25 @@ main(int argc, char*argv[])
 */
 
     SpaceId newProc;
-    OpenFileId input = ConsoleInput;
-    OpenFileId output = ConsoleOutput; 
-    OpenFileId fd;
+    OpenFileId fd, out;
+    OpenFileId input;
+    OpenFileId output; 
+
     char filename[30];
+    char redirFile[30];
     char execBuffer[16][30];
     char scriptBuf[8];
 
     char prompt[2], buffer[120];
-    int i, j, script;
+    int i, j, script, redir;
     int exitval;
     int argcount;  
     char *args[16];
     
+    input = ConsoleInput;
+    output = ConsoleOutput;
     script=0;
+    redir=0;
     argcount=0;
     prompt[0] = '-';
     prompt[1] = '-';
@@ -106,7 +111,8 @@ main(int argc, char*argv[])
             Write(prompt, 2, output);
         }
         i = 0;
-        
+        redir=0;
+        script=0;
         do {
         
          Read(&buffer[i], 1, input);
@@ -114,6 +120,8 @@ main(int argc, char*argv[])
         } while( buffer[i++] != '\n' );
 
         buffer[--i] = '\0';
+        /*prints(buffer, output);
+        prints("\n", output);*/
 
         argcount=0;
         /*Check for a # and treat as Comment as in do not proceed*/
@@ -122,12 +130,11 @@ main(int argc, char*argv[])
         /* Get the Process Name */
             i=0;
             while(buffer[i]!=' ' && buffer[i]!='\0'){
-                if(i>=30){break;}
                 filename[i] = buffer[i];
                 i++;
             }
             filename[i] = '\0';
-            /*prints(filename, output);*/
+           
 
             /* Open the File and see if it is a script */
             fd = Open(filename);
@@ -139,47 +146,79 @@ main(int argc, char*argv[])
             }
             else{
                 j = Read(scriptBuf, 7, fd);
-                prints(scriptBuf, output);
-                
-                if(j==7 && scriptBuf[0] == '#' && scriptBuf[1] == 'S' && scriptBuf[2] == 'C' && scriptBuf[3] == 'R' && scriptBuf[0] == 'I' && scriptBuf[0] == 'P' && scriptBuf[0] == 'T' ){/* Scripting Section */
+                if(j==7 && scriptBuf[0] == '#' && scriptBuf[1] == 'S' && scriptBuf[2] == 'C' && scriptBuf[3] == 'R' && scriptBuf[4] == 'I' && scriptBuf[5] == 'P' && scriptBuf[6] == 'T' ){/* Scripting Section */
                     /*   If the file is a script then we want to */
                     /* dup the file into the input and exec a    */
                     /* new shell process with a flag telling it  */
                     /* is a script                               */
-                    prints("Hey I'm a Script!", output);
+                    
+                    
                     newProc = Fork();
                     if(newProc == 0){
+                        j=0;
+                       /* if(buffer[i]!='\0'){
+                                while(buffer[i]==' '){i++;}
+                                if(buffer[i]=='>'){
+                                    i++;
+                                    while(buffer[i]==' '){i++;}
+                                    while(buffer[i]!= '\0'){
+                                        redirFile[j]=buffer[i];
+                                        i++;j++;
+                                    }
+                                    redirFile[j]='\0';
+                                    out = Open(redirFile);
+                                    prints("I am here\n", output);
+                                    if(out==-1){
+                                        Create(redirFile);
+                                        out = Open(redirFile);
+                                    }
+                                    prints("now here\n", output);
+                                    Close(ConsoleOutput);
+                                    Dup(out);
+                                    prints("lslsdkld", 1);
+                                    Close(out);
+
+                                }
+                                
+                        }*/
+                        Close(fd);
+                        fd = Open(filename);
                         Close(ConsoleInput);
                         Dup(fd);
                         Close(fd);
-                        Exec(filename, "smode");
-
-                        /* Building Arg Sting */
-
-                        
+                        args[0]="smode";
+                        args[1]=(char*)0;
+                        Exec("shell", args);
+                    }
+                    else if(newProc == -1){
+                        prints("well fuck\n", output);
                     }
                     else{
+                        prints("waiting for the script to finish", output);
                         Join(newProc);
 
                     }
+                    prints("heyo\n", output);
                     Close(fd);
                 }
                 else {/* Exec With Args */
                     Close(fd);
                     newProc = Fork();
                     if (newProc == 0) {      
-                        i=0,j=0;                               
+                        i=0;j=0;                               
                         while(buffer[i] != '\0'){/* Gets the Args and puts them in the argv */
-                            Write("<", 1, output);
+/*                            Write("<", 1, output);
                             Write(&buffer[i], 1, output);
-                            Write(">", 1, output);
-                            if(buffer[i]==' '){
+                            Write(">", 1, output);*/
+                            if(buffer[i]==' '||buffer[i+1]=='\0'){
+                                if(buffer[i+1]=='\0'){execBuffer[argcount][j] = buffer[i];j++;}
                                 execBuffer[argcount][j] = '\0';
-                                prints(execBuffer[argcount], output);
                                 while(buffer[i+1]==' '){i++;}/* Nasty Stuff, strips off extra spaces and increments i */
+                                /*if(buffer[i+1]=='>'){i++;redir=1;while(buffer[i+1]==' '){i++;}i++;}*/
                                 argcount++;
                                 if(argcount>15){Write("\nToo Many Arguments\n\n", 21, output ); Exit(-1);}
                                 j=0;
+                                if(redir){break;}
                             }
                             else{
                                 execBuffer[argcount][j] = buffer[i];
@@ -187,22 +226,47 @@ main(int argc, char*argv[])
                             }
                             i++;
                         }
+                        /*if(redir){
+                            j=0;
+                            while(buffer[i]!= '\0'){
+                                redirFile[j]=buffer[i];
+                                i++;j++;
+                            }
+                            redirFile[j]='\0';
+                            prints(redirFile, output);
+                            out = Open(redirFile);
+                            prints("I am here\n", output);
+                            if(out==-1){
+                                Create(redirFile);out = Open(redirFile);
+                            }
+                            prints("now here\n", output);
+                            Close(ConsoleOutput);
+                            Dup(out);
+                            prints("lslsdkld", 1);
+                            Close(out);
+                        }
+                        redir=0;
+                        */
                         for(i=0;i<=argcount;i++){/*Put the args into the *char[] to pass into exec*/
                             args[i]=execBuffer[i];
                         }
-
-                        args[argcount+1]=(char *)0;/*Put a \0 in the last arg spot*/
-
-                        Exec(args[0], args);
+                        args[argcount]=(char *)0;/*Put a \0 in the last arg spot*/
+                        prints("I say heyhey", 1);
+                        Exec(args[0], &args[1]);
                         Halt();
+                        
+                        
+                    }
+                     else if(newProc == -1){
+                        prints("well fuck\n", output);
                     }
                     else {
-                        
+                        prints("parent waiting\n", output);
                         exitval = Join(newProc);
                         Write("\nProcess Exited: ", 17, output);
                         printd(exitval, output);
                         Write("\n\n", 2, output);
-
+                        if(script){Exit(0);}
 
                     }
                 } 
