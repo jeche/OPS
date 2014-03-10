e8eebbe80c0cc1a394b00b67de51b57506c8fdbd
jeche <jlchen@email.wm.edu>
2014-03-10 18:33:57 -0400

    network test skeletons added

---
+++ nettest/server.c
@@ -0,0 +1,68 @@
+/* server.c
+ *
+ */
+
+#include "syscall.h"
+
+int
+main()
+{
+  while(1){
+    /* Receive connections of machine id and mailbox number*/
+    /* Mark a machine as waiting, if one is not waiting, if not waiting send back machine to connect to with mailbox*/
+    /* Send back another machine who wants to play */
+  }
+}
+
+/* Print a null-terminated string "s" on open file descriptor "file". */
+
+prints(s,file)
+char *s;
+OpenFileId file;
+
+{
+  int count = 0;
+  char *p;
+
+  p = s;
+  while (*p++ != '\0') count++;
+  Write(s, count, file);  
+
+}
+
+
+/* Print an integer "n" on open file descriptor "file". */
+
+printd(n,file)
+int n;
+OpenFileId file;
+
+{
+
+  int i, pos=0, divisor=1000000000, d, zflag=1;
+  char c;
+  char buffer[11];
+  
+  if (n < 0) {
+    buffer[pos++] = '-';
+    n = -n;
+  }
+  
+  if (n == 0) {
+    Write("0",1,file);
+    return;
+  }
+
+  for (i=0; i<10; i++) {
+    d = n / divisor; n = n % divisor;
+    if (d == 0) {
+      if (!zflag) buffer[pos++] =  (char) (d % 10) + '0';
+    } else {
+      zflag = 0;
+      buffer[pos++] =  (char) (d % 10) + '0';
+    }
+    divisor = divisor/10;
+  }
+  Write(buffer,pos,file);
+}
+
