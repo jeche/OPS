3541ab07c46aa372fb2ca949df3e8969dd08ab6c
Sara-lily <jlchen@email.wm.edu>
2014-03-21 17:52:13 -0400

    adding network tests

--- test/send.c
+++ test/send.c
@@ -10,8 +10,42 @@ int
 main(int argc, char **argv)
 {
   char *args;
-  Recv(args, 6, 1);
+  int mailbox = GetMailbox();
+  Recv(args, 6, mailbox);
   Write(args, 6, ConsoleOutput);
   Halt();
   /* not reached */
+}
+
+printd(n,file)
+int n;
+OpenFileId file;
+
+{
+
+  int i, pos=0, divisor=1000000000, d, zflag=1;
+  char c;
+  char buffer[11];
+  
+  if (n < 0) {
+    buffer[pos++] = '-';
+    n = -n;
+  }
+  
+  if (n == 0) {
+    Write("0",1,file);
+    return;
+  }
+
+  for (i=0; i<10; i++) {
+    d = n / divisor; n = n % divisor;
+    if (d == 0) {
+      if (!zflag) buffer[pos++] =  (char) (d % 10) + '0';
+    } else {
+      zflag = 0;
+      buffer[pos++] =  (char) (d % 10) + '0';
+    }
+    divisor = divisor/10;
+  }
+  Write(buffer,pos,file);
 }
